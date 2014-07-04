cbfb10986b0c9a164b566433caddc56f81e36582
Musshi <musshi@rubify.com>
2014-07-04 16:18:50 +0700

    Add: Token file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UploadToBox::Application.config.secret_key_base = '467e8328803ac21ae561a6c66cdb212b7ac821448c013f3c38f42ed979d2285f9cc42511b97f7418dbd3efeaee16e8ae21b1f02e45406abe0e1ac95ff2783eb7'
